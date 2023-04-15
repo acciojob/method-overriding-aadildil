0552f2e38aee2d224d6942aee4d198fadf84ea99
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-15 11:06:36 +0530

    method_overriding_1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,5 +5,19 @@ public class Main {
         B b=new B();
         System.out.println(b.meth());
     }
-  
+  public static class A
+  {
+      public String meth()
+      {
+          return "Invoking method from class A";
+      }
+  }
+    public static class B extends A{
+        @Override
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
 }
